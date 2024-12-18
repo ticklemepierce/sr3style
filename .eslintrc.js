afa6b29ec9bc0384c7f803042b2a91989f3cd93a
ticklemepierce <pierce.tickle@gmail.com>
2024-12-17 22:53:01 -0500

    single quotes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2021,
-    sourceType: "module",
+    sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
     },
@@ -14,52 +14,53 @@ module.exports = {
   },
   root: true,
   extends: [
-    "eslint:recommended",
-    "prettier",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:prettier/recommended",
-    "plugin:react-hooks/recommended",
+    'eslint:recommended',
+    'prettier',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended',
   ],
-  plugins: ["prettier", "@typescript-eslint", "react", "react-hooks"],
+  plugins: ['prettier', '@typescript-eslint', 'react', 'react-hooks'],
   rules: {
     // JavaScript rules
-    "prefer-const": "warn",
-    "no-var": "warn",
-    "no-unused-vars": "warn",
-    "object-shorthand": "warn",
-    "quote-props": ["warn", "as-needed"],
+    'prefer-const': 'warn',
+    'no-var': 'warn',
+    'no-unused-vars': 'warn',
+    'object-shorthand': 'warn',
+    'quote-props': ['warn', 'as-needed'],
     // TypeScript rules
-    "@typescript-eslint/array-type": [
-      "warn",
+    '@typescript-eslint/array-type': [
+      'warn',
       {
-        default: "array",
+        default: 'array',
       },
     ],
-    "@typescript-eslint/consistent-type-assertions": [
-      "warn",
+    '@typescript-eslint/consistent-type-assertions': [
+      'warn',
       {
-        assertionStyle: "as",
-        objectLiteralTypeAssertions: "never",
+        assertionStyle: 'as',
+        objectLiteralTypeAssertions: 'never',
       },
     ],
     // React rules
-    "react/jsx-fragments": ["warn", "syntax"], // Shorthand syntax for React fragments
-    "react/jsx-filename-extension": [
-      "warn",
+    'react/jsx-fragments': ['warn', 'syntax'], // Shorthand syntax for React fragments
+    'react/jsx-filename-extension': [
+      'warn',
       {
-        extensions: ["ts", "tsx"],
+        extensions: ['ts', 'tsx'],
       },
     ],
-    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
-    "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies
-    "react/react-in-jsx-scope": "off",
-    "react/prop-types": "off",
-    "prettier/prettier": "warn",
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'prettier/prettier': 'warn',
+    'prettier/prettier': ['error', { singleQuote: true }],
   },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
 };
